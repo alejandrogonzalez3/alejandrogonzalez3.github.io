8a307b937500e622fa183721696d8982edad4630
Alejandro González Martínez <alejandro.gonzalez@trabesoluciones.net>
2022-06-16 11:06:27 +0200

    Updates

---
+++ precache-manifest.0dd2a5c06dfe685b0662d5bb4fc1155a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd0100faff8978b81849a6394d4c740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5228851e6458ca2c4875",
+    "url": "/static/css/main.7190c8e8.chunk.css"
+  },
+  {
+    "revision": "38774824d486d3220b70",
+    "url": "/static/js/2.eb5b461d.chunk.js"
+  },
+  {
+    "revision": "9901e7c3e6ada12bd25cdfbc4078f150",
+    "url": "/static/js/2.eb5b461d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5228851e6458ca2c4875",
+    "url": "/static/js/main.d99ed20e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d99ed20e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b3f776a8565767cc5b",
+    "url": "/static/js/runtime-main.93f61b27.js"
+  },
+  {
+    "revision": "e2704ee70ffbf70a61b40c02a05d4e49",
+    "url": "/static/media/alejandrogonzalez.e2704ee7.png"
+  }
+]);
